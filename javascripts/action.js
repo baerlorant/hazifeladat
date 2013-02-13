c4ee2f7ec76aa90ec4d3fac33bc890b294e0e0a5
Lorant Baer <lory01sec@gmail.com>
2013-02-13 19:14:37 +0100

    Update javascripts/action.js

--- javascripts/action.js
+++ javascripts/action.js
@@ -1,11 +1,11 @@
 function keyup(field) {
             var last = field.value.charAt( field.value.length-1 );
             if( last == "s"){
-                alert("A teljel szöveg: " + field.value);
+                //alert("A teljel szöveg: " + field.value);
                 document.getElementById('eredmeny').value = field.value;
             }
             else{
-                alert("A leütött karakter:" + last);
+                //alert("A leütött karakter:" + last);
                 document.getElementById('eredmeny').value = last;
             }
         }

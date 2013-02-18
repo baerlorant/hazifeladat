94195aeb251253221e05795c51d04b090ec132f4
Lorant Baer <lory01sec@gmail.com>
2013-02-18 18:22:47 +0100

    Update javascripts/action.js

--- javascripts/action.js
+++ javascripts/action.js
@@ -69,3 +69,26 @@ var myHandler = (function () {
 
 document.addEventListener('DOMContentLoaded', myHandler);
 
+
+function $_GET(q,s) {
+        s = s ? s : window.location.search;
+        var re = new RegExp('&'+q+'(?:=([^&]*))?(?=&|$)','i');
+        return (s=s.replace(/^?/,'&').match(re)) ? (typeof s[1] == 'undefined' ? '' : decodeURIComponent(s[1])) : undefined;
+}
+
+function changefizz(field) {
+            var out = '';
+            if( field.value % 3 == 0)
+                out = out.'fiz';
+            if( field.value % 5 == 0)
+                out = out.'buzz';
+            if( field.value % 7 == 0)
+                out = out.'bizz';
+            
+            document.getElementById('fizz').value = out;
+            
+}
+
+
+    
+

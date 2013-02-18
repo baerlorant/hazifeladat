443b0adfce8f4a95318e25c1c182e29f7ba2c339
Lorant Baer <lory01sec@gmail.com>
2013-02-18 10:13:09 +0100

    Update javascripts/action.js

--- javascripts/action.js
+++ javascripts/action.js
@@ -1,4 +1,4 @@
-function keyup(field) {
+function keyup1(field) {
             var last = field.value.charAt( field.value.length-1 );
             ///var c= String.fromCharCode(field.keyCode);
             //var last = c.match(/\w/);
@@ -10,4 +10,32 @@ function keyup(field) {
                 alert("A leütött karakter:" + last);
                 document.getElementById('eredmeny').value = last;
             }
-        }
+}
+
+function keyup2(field) {
+            var last = field.value.charAt( field.value.length-1 );
+            ///var c= String.fromCharCode(field.keyCode);
+            //var last = c.match(/\w/);
+            if( last == "s"){
+                alert("A teljel szöveg: " + field.value);
+                document.getElementById('eredmeny').value = field.value;
+            }
+            else{
+                alert("A leütött karakter:" + last);
+                document.getElementById('eredmeny').value = last;
+            }
+}
+
+function keyup3(field) {
+            var last = field.value.charAt( field.value.length-1 );
+            ///var c= String.fromCharCode(field.keyCode);
+            //var last = c.match(/\w/);
+            if( last == "s"){
+                alert("A teljel szöveg: " + field.value);
+                document.getElementById('eredmeny').value = field.value;
+            }
+            else{
+                alert("A leütött karakter:" + last);
+                document.getElementById('eredmeny').value = last;
+            }
+}

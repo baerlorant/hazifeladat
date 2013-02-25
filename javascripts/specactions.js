6ba47456e26e7be9a47313b7ad30f54fc39c813f
Lorant Baer <lory01sec@gmail.com>
2013-02-25 09:26:17 +0100

    Update javascripts/specactions.js

--- javascripts/specactions.js
+++ javascripts/specactions.js
@@ -1,13 +1,14 @@
 describe("changefizz", function(){
   var i = 0;
   var element = document.createElement("input");
+
   element.setAttribute("id", "fizz");
   element.setAttribute("type", "text");
   
-  beforeEach(function() {
+  /*beforeEach(function() {
      i =  i + 1;
      document.getElementById("fizz").value = i;
-  });
+  });*/
 
   for(var j=0;j<100;j++){
       it("Test for: " + i, function() {
